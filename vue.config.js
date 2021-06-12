0291d177b8209b5d7b2e94ae949dbc5dbab0af34
qscftyjm1234 <qscftyjm1234@gmail.com>
2021-06-12 21:52:19 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,5 @@ module.exports = {
   // },
 
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/red_house_project/' : '/',
+    process.env.NODE_ENV === 'production' ? '/red_house_project/dist/' : '/',
 };
