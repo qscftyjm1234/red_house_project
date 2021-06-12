9e617b147972e6825e4b3735a720b4d9917e8fec
qscftyjm1234 <qscftyjm1234@gmail.com>
2021-06-12 21:49:44 +0800

    dist

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // build: {
+  //   index: path.resolve(__dirname, '../dist/index.html'),
+
+  //   assetsRoot: path.resolve(_dirname, '../dist'),
+  //   assetsSubDirectory: 'js',
+  //   assetsPublicPath: '/red_house_projext/dist/',
+  // },
+
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/red_house_project/' : '/',
+};
