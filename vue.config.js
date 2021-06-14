747bfff4efad53cded56070276bc5f921de25485
qscftyjm1234 <qscftyjm1234@gmail.com>
2021-06-14 21:59:42 +0800

    珊東西

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,4 @@
 module.exports = {
-  // build: {
-  //   index: path.resolve(__dirname, '../dist/index.html'),
-
-  //   assetsRoot: path.resolve(_dirname, '../dist'),
-  //   assetsSubDirectory: 'js',
-  //   assetsPublicPath: '/red_house_projext/dist/',
-  // },
-
   publicPath:
     process.env.NODE_ENV === 'production' ? '/red_house_project/' : '/',
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       data: `
-  //            @import "@/assets/css/app.scss";
-  //         `,
-  //     },
-  //   },
-  // },
 };
